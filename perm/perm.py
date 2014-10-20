64fd2ee20bfff36b1cc69013758830b7525b3117
Ethan Hart <ethan.john.hart@gmail.com>
2014-10-20 16:09:55 -0700

    Make perm.py pep8 compliant

--- perm/perm.py
+++ perm/perm.py
@@ -4,7 +4,8 @@
 """
 Given: A positive integer n≤7.
 
-Return: The total number of permutations of length n, followed by a list of all such permutations (in any order).
+Return: The total number of permutations of length n, followed by a list of all
+such permutations (in any order).
 """
 
 from sys import argv
