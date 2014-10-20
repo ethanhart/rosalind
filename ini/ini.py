eee30630b233bbb649561492839b78802bc39f72
Ethan Hart <ethan.john.hart@gmail.com>
2014-10-20 16:54:10 -0700

    Add solution for INI: Intro to Bioinfo Armory

---
+++ ini/ini.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+Given: A DNA string s of length at most 1000 bp.
+
+Return: Four integers (separated by spaces) counting the respective number of
+        times that the symbols 'A', 'C', 'G', and 'T' occur in s.
+"""
+
+from sys import argv
+
+__author__ = "Ethan Hart"
+
+
+def read_data(data_file):
+    with open(data_file, 'r') as inf:
+        lines = inf.read()
+    return lines
+
+
+def count_dna_strings(data):
+    """
+    Count instances of DNA nucleotides in given string
+    Set: A C G T
+    """
+
+    strings = ["A", "C", "G", "T"]
+    counts = map(lambda s: str(data.count(s)), strings)
+    return ' '.join(counts)
+
+
+def main():
+    data = argv[1]
+    datalines = read_data(data)
+    print count_dna_strings(datalines)
+
+
+if __name__ == "__main__":
+    main()
